760b3aac680c83481cfaf59c6717c7bba171af55
Elihofni Lima <elihofnil@id.uff.br>
2018-06-16 21:35:59 -0300

    dummy data generator WIP

--- modeloTrabalho.sql
+++ modeloTrabalho.sql
@@ -1,4 +1,4 @@
-﻿-------------------------------------- DROPS --------------------------------------------------------------
+﻿------------------------------------------------ DROPS --------------------------------------------------------------
 DROP TABLE IF EXISTS CURSOS CASCADE;
 
 DROP TABLE IF EXISTS DISCIPLINAS CASCADE;
@@ -10,6 +10,8 @@ DROP TABLE IF EXISTS PROFESSORES CASCADE;
 DROP TABLE IF EXISTS DISCIPLINAS_LECIONAVEIS CASCADE;
 
 DROP TABLE IF EXISTS ALUNOS cascade;
+
+DROP TABLE IF EXISTS ALUNOS_INSCRITOS cascade;
 
 DROP TABLE IF EXISTS TURMAS CASCADE;
 
@@ -40,9 +42,9 @@ CREATE TABLE PROFESSORES (
 
 
 CREATE TABLE DISCIPLINAS_LECIONAVEIS (
-    id  serial,
-    professor integer not null,
-    disciplina integer not null,
+    id          serial,
+    professor   integer not null,
+    disciplina  integer not null,
     UNIQUE (professor, disciplina),
     CONSTRAINT pk_lecionavel PRIMARY KEY (id)
 );
@@ -77,29 +79,27 @@ CREATE TABLE SALAS (
 );
 
 
+CREATE TABLE OFERTAS (
+    id                      serial,
+    semestre                character(5) not null,          -- 2018.1
+    disciplina_oferecida    integer not null,               -- 123
+    vagas                   integer not null default 30,    -- 30
+    alunos_inscritos        integer not null default 0,     -- 20
+    UNIQUE(semestre, disciplina_oferecida),
+    CONSTRAINT fk_disciplina_oferecida FOREIGN KEY (disciplina_oferecida) REFERENCES DISCIPLINAS_OFERECIDAS(id),
+    CONSTRAINT pk_oferta PRIMARY KEY (id)
+);
+
+
 CREATE TABLE TURMAS (
     id              serial,                 -- 1
     codigo_turma    character(2) not null,  -- A1
     professor       integer not null,       -- Luiz André
-    disciplina      integer not null,       -- 12312 
-    UNIQUE (codigo_turma, professor, disciplina),
+    oferta          integer not null,       -- 12312 
+    UNIQUE (codigo_turma, professor, oferta),
     CONSTRAINT fk_professor foreign key (professor) references PROFESSORES(id),
-    CONSTRAINT fk_disciplina foreign key (disciplina) references DISCIPLINAS(id),
+    CONSTRAINT fk_oferta foreign key (oferta) references OFERTAS(id),
     CONSTRAINT pk_turma PRIMARY KEY (id)
-);
-
-
-CREATE TABLE OFERTAS (
-    id                  serial,
-    semestre            character(5) not null,          -- 2018.1
-    disciplina          integer not null,               -- 123
-    turma               integer not null,               -- 1234
-    vagas               integer not null default 30,    -- 30
-    alunos_inscritos    integer not null default 0,     -- 20
-    UNIQUE(semestre, disciplina, turma),
-    CONSTRAINT fk_turma FOREIGN KEY (turma) REFERENCES TURMAS(id),
-    CONSTRAINT fk_disciplina FOREIGN KEY (disciplina) REFERENCES DISCIPLINAS(id),
-    CONSTRAINT pk_oferta PRIMARY KEY (id)
 );
 
 
@@ -115,10 +115,12 @@ CREATE TABLE ALUNOS (
 
 
 CREATE TABLE ALUNOS_INSCRITOS (
-    id  serial,
+    id      serial,
     aluno   integer not null,
     oferta  integer not null,
     UNIQUE (aluno, oferta),
+    CONSTRAINT fk_aluno FOREIGN KEY (aluno) REFERENCES ALUNOS (id),
+    CONSTRAINT fk_oferta FOREIGN KEY (oferta) REFERENCES OFERTAS (id),
     CONSTRAINT pk_inscricao PRIMARY KEY (id)
 );
 
@@ -136,33 +138,167 @@ CREATE TABLE AULAS (
 );
 
 
--- Semestre nas duas tabelas?
--- Como garantir a inscrição de um aluno ou não de outro?
--- Talvez remover INSCRIÇÕES e SOLICITAÇÕES por conta do histórico do aluno e CR. Como calcular?
-/* 
-CREATE TABLE inscricoes (
-    id                  SERIAL,
-    semestre            character(5) not null,  -- 2018.1
-    matricula_aluno     bigint not null,        -- 2140131126
-    codigo_turma        integer not null,       -- 1 
-    unique (semestre, matricula_aluno, codigo_turma),
-    constraint fk_aluno foreign key (matricula_aluno) references aluno(matricula),
-    constraint fk_turma foreign key (codigo_turma) references turma(id),
-    constraint pk_inscricoes primary key (id)
-);
 
-CREATE TABLE leciona_em(
-    id          SERIAL,
-    professor   integer not null,       -- 1
-    turma       integer not null,       -- 1
-    semestre    character(5) not null,  -- 2018.1
-    unique (professor, turma, semestre),
-    constraint fk_professor foreign key (professor) references professor(id),
-    constraint fk_professor_turma foreign key (turma) references turma(id), 
-    constraint pk_leciona_em primary key (id)
-);
+--------------------------------------- DUMMY DATA INSERTS ---------------------------------------------------------
+-- DISCIPLINAS
+DROP FUNCTION IF EXISTS gera_disciplinas();
+CREATE OR REPLACE FUNCTION gera_disciplinas() RETURNS void AS $$
+DECLARE
+    maxValue integer = round(random()*(100 + 5)); -- Entre 5 e 100
+    codigo text;
+    nome text;
+    ementa text;
+    cargaHoraria integer;
+BEGIN
+    FOR i IN 1..maxValue LOOP
+        codigo = 'TCC00' || i;
+        nome = 'disciplina-' || i;
+        ementa = 'Aprendizado em ' || i || ' e ' || i * 2;
+        cargaHoraria = ((i % 2) + 1) * 32;
+        INSERT INTO DISCIPLINAS(cod_disciplina, nome, ementa, carga_horaria) VALUES (codigo, nome, ementa, cargaHoraria);
+    END LOOP;
+END;$$ LANGUAGE plpgsql;
+
+SELECT gera_disciplinas();
+SELECT * FROM DISCIPLINAS;
+
+
+-- PROFESSORES
+DROP FUNCTION IF EXISTS gera_professores();
+CREATE OR REPLACE FUNCTION gera_professores() RETURNS void AS $$
+DECLARE
+    maxValue integer = round(random()*(100 + 5)); -- Entre 5 e 100
+    nmProfessor text;
+    emailProfessor text;
+BEGIN
+    FOR i IN 1..maxValue LOOP
+        nmProfessor = 'professor-' || i;
+        emailProfessor = 'professor-' || i || '@id.uff.br';
+        INSERT INTO PROFESSORES(matricula, nome, email) VALUES (2018000 + i, nmProfessor, emailProfessor);
+    END LOOP;
+END;$$ LANGUAGE plpgsql;
+
+SELECT gera_professores();
+SELECT * FROM PROFESSORES;
+
+
+-- DISCIPLINAS_LECIONAVEIS
+DROP FUNCTION IF EXISTS gera_disciplinas_lecionaveis();
+CREATE OR REPLACE FUNCTION gera_disciplinas_lecionaveis() RETURNS void AS $$
+DECLARE
+    maxValueProfessores integer = (SELECT COUNT(*) FROM PROFESSORES);
+    maxValueDisciplinas integer = (SELECT COUNT(*) FROM DISCIPLINAS);
+BEGIN
+    FOR i IN 1..maxValueProfessores LOOP
+        INSERT INTO DISCIPLINAS_LECIONAVEIS(professor, disciplina) VALUES (i, round(random()*(maxValueDisciplinas + 1) + 1));
+    END LOOP;
+END;$$ LANGUAGE plpgsql;
+SELECT gera_disciplinas_lecionaveis();
+SELECT * FROM DISCIPLINAS_LECIONAVEIS;
+
+
+-- CURSOS                            
+DROP FUNCTION IF EXISTS gera_cursos();
+CREATE OR REPLACE FUNCTION gera_cursos() RETURNS void AS $$
+DECLARE
+    maxValue integer = (SELECT COUNT(*) FROM PROFESSORES) / 2;
+    curso text;
+BEGIN
+    FOR i IN 1..maxValue LOOP
+        curso = 'curso-' || i;
+        INSERT INTO CURSOS(nome, professor_coordenador, vice_coordenador) VALUES (curso, i, (maxValue * 2) - i);
+    END LOOP;
+END;$$ LANGUAGE plpgsql;
+
+SELECT gera_cursos();
+SELECT * FROM CURSOS;
+
+
+-- DISCIPLINAS_OFERECIDAS
+DROP FUNCTION IF EXISTS gera_disciplinas_oferecidas();
+CREATE OR REPLACE FUNCTION gera_disciplinas_oferecidas() RETURNS void AS $$
+DECLARE
+    maxValueCursos integer = (SELECT COUNT(*) FROM CURSOS);
+    maxValueDisciplinas integer = (SELECT COUNT(*) FROM DISCIPLINAS);
+BEGIN
+    FOR i IN 1..maxValueCursos LOOP
+        INSERT INTO DISCIPLINAS_OFERECIDAS(curso, disciplina) VALUES (i, round(random()*(maxValueDisciplinas + 1) + 1));
+    END LOOP;
+END;$$ LANGUAGE plpgsql;
+SELECT gera_disciplinas_oferecidas();
+SELECT * FROM DISCIPLINAS_OFERECIDAS;
+
+
+-- SALAS                             
+DROP FUNCTION IF EXISTS gera_salas();
+CREATE OR REPLACE FUNCTION gera_salas() RETURNS void AS $$
+DECLARE
+    numero integer;
+    salas integer[] = array[0, 2, 4, 6, 8, 10, 11, 13, 15, 17, 19];
+BEGIN
+    FOR andar IN 2..3 LOOP
+        FOR num IN 1..array_length(salas, 1) LOOP
+            numero = (100 * andar) + salas[num];
+            INSERT INTO SALAS(numero, bloco) VALUES (numero, 'IC1');
+            INSERT INTO SALAS(numero, bloco) VALUES (numero, 'IC2');
+        END LOOP;
+    END LOOP;
+END;$$ LANGUAGE plpgsql;
+
+SELECT gera_salas();
+SELECT * FROM SALAS;
+
+/*
+-- TURMAS
+DROP FUNCTION IF EXISTS gera_turmas();
+CREATE OR REPLACE FUNCTION gera_turmas() RETURNS void AS $$
+DECLARE
+    maxValueProfessores integer = (SELECT COUNT(*) FROM PROFESSORES); 
+    professor integer;
+    codigos char[] = '{A, B, C, D, E}';
+    d record;
+    codigo char(2);
+BEGIN
+    FOR d IN SELECT * FROM DISCIPLINAS_OFERECIDAS LOOP
+        FOR i IN 1..round(random()*4 + 1) LOOP -- De 1 à 5 turmas por disciplina
+            professor = round(random()*(maxValueProfessores - 1)+1);
+            codigo = (codigos[i]) || i;
+            INSERT INTO TURMAS(codigo_turma, professor, oferta) VALUES (codigo, professor, oferta);
+        END LOOP;
+    END LOOP;
+END;$$ LANGUAGE plpgsql;
+
+SELECT gera_turmas();
+SELECT * FROM TURMAS;
+
+-- OFERTAS
+semestre        
+disciplina      
+turma           
+vagas           
+alunos_inscritos
+DROP FUNCTION IF EXISTS gera_ofertas();
+CREATE OR REPLACE FUNCTION gera_ofertas() RETURNS void AS $$
+DECLARE
+    maxValueProfessores integer = (SELECT COUNT(*) FROM PROFESSORES); 
+    professor integer;
+    codigos char[] = '{A, B, C, D, E}';
+    d record;
+    codigo char(2);
+BEGIN
+    FOR d IN SELECT * FROM DISCIPLINAS_OFERECIDAS LOOP
+        FOR i IN 1..round(random()*4 + 1) LOOP -- De 1 à 5 turmas por disciplina
+            professor = round(random()*(maxValueProfessores - 1)+1);
+            codigo = (codigos[i]) || i;
+            INSERT INTO TURMAS(codigo_turma, professor, disciplina) VALUES (codigo, professor, d.disciplina);
+        END LOOP;
+    END LOOP;
+END;$$ LANGUAGE plpgsql;
+
+SELECT gera_ofertas();
+SELECT * FROM OFERTAS;
+
 */
-
 -------------------------------------- TRIGGERS --------------------------------------------------------------
 
 --A ideia do trigger abaixo é catar se o professor possui mais de duas turmas em um mesmo horário
